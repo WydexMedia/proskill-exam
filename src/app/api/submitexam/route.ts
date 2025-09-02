296c7f3788f663795ba704cd540a8ec8aa49dd51
minhajputhiyara <minhajputhiyara@gmail.com>
2025-09-02 13:16:41 +0530

    fix

--- src/app/api/submitexam/route.ts
+++ src/app/api/submitexam/route.ts
@@ -3,10 +3,6 @@
 import { NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
 import nodemailer from "nodemailer";
-import fs from "fs";
-import path from "path";
-import { PDFDocument, rgb, StandardFonts } from "pdf-lib";
-import * as fontkit from 'fontkit';
 import { generateCertificatePDF, normalizeNameForCursive } from "@/lib/certificate";
 
 
@@ -28,48 +24,7 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-async function generateCertificatePDF(name: string, dateStr: string) {
-  const certImagePath = path.join(process.cwd(), "public/certificate.jpg");
-  const certImageBytes = fs.readFileSync(certImagePath);
-
-  const fontPath = path.join(process.cwd(), "public/fonts/FormaleScript_PERSONAL_USE_ONLY.otf");
-  const customFontBytes = fs.readFileSync(fontPath);
-
-  const pdfDoc = await PDFDocument.create();
-  pdfDoc.registerFontkit(fontkit as any); // Register fontkit
-
-  const page = pdfDoc.addPage([842, 595]);
-
-  const jpgImage = await pdfDoc.embedJpg(certImageBytes);
-  page.drawImage(jpgImage, {
-      x: 0,
-      y: 0,
-      width: 842,
-      height: 595,
-  });
-
-  const cursiveFont = await pdfDoc.embedFont(customFontBytes);
-
-  page.drawText(name, {
-      x: 350,
-      y: 200,
-      size: 28,
-      font: cursiveFont,
-      color: rgb(0, 0, 0),
-  });
-
-  const standardFont = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
-  page.drawText(dateStr, {
-      x: 255,
-      y: 70,
-      size: 18,
-      font: standardFont,
-      color: rgb(0, 0, 0),
-  });
-
-  const pdfBytes = await pdfDoc.save();
-  return pdfBytes;
-}
+// using generateCertificatePDF from @/lib/certificate
 
 // Helper to render answers as HTML table (only attended answers)
 function renderAnswersTable(answers: Record<string, string>) {
